9d2a8a5a5356f61bad0376a26d57cd0267e4bea9
Donovan <donovanvdl@Infoslips.com>
2020-05-04 18:27:56 +0200

    added build

---
+++ build/precache-manifest.5bd861a24ef5dcf720dac9556cd77907.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "95c0ff77de9a2f8038c5",
+    "url": "/static/js/main.95c0ff77.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "95c0ff77de9a2f8038c5",
+    "url": "/static/css/main.f1fbde7a.chunk.css"
+  },
+  {
+    "revision": "73f3a342b6e3dbd65392091d8198212d",
+    "url": "/index.html"
+  }
+];
